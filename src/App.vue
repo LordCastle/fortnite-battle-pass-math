9c3a9266a6319bb2451066b21ec3c99dc8dcbc20
Chris Castle <CcastleX@gmail.com>
2021-03-08 19:55:39 -0600

    Styling updates
    - Updated some base styling around background-colors and font-colors mostly.

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,26 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+    box-sizing: border-box;
+}
+
+body {
+    background-color: #2a2a2a;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+}
+
+strong {
+    color: #ff0;
+    font-size: 1.1rem;
 }
 </style>
